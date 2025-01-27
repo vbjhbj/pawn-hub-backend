5ca862aeb531dfbf6184e59429810522f7fe134d
vbjhbj <119402788+vbjhbj@users.noreply.github.com>
2025-01-27 10:40:54 +0100

    Routes

--- routes/api.php
+++ routes/api.php
@@ -37,3 +37,76 @@ Route::middleware('auth:sanctum')->get('items/{shopID}/
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->get('item/{itemID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('shops/
+	?page="{page}"
+	&searchKey="{key}"
+	&orderBy="{orderBy}"
+	&asc="{order}"
+	&hold="{holding}"
+	&settlements="{setlList}"', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('customers/{shopID}/
+	?page="{page}"
+	&searchKey="{key}"
+	&searchIn="{searchParam}"
+	&orderBy="{orderBy}"
+	&asc="{order}"
+	&status="{status}"', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('shop/{userID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('customer/{userID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('customerNonuser/{customerID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('loans/
+	?page="{page}"
+	&searchKey="{key}"
+	&searchIn="{searchParam}"
+	&orderBy="{orderBy}"
+	&asc="{order}"
+	&status="{status}"', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('loan/{loanID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('messages/?sender="{sId}"&receiver="{rId}"', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->get('message/{messageID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->set('item/{itemID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->set('shop/{userID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->set('loan/{loanID}', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->set('setting/{settingID}', function (Request $request) {
+    return $request->user();
+});
